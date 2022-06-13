118afda615562d389dbf33ed00d96cd6649ae714
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-06-13 10:46:39 +0530

    Done mark7

---
+++ script.js
@@ -0,0 +1,27 @@
+const button = document.querySelector("#submit");
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+button.addEventListener("click", () => {
+  setTranslate(input.value);
+});
+
+input.addEventListener("input", (e) => {
+  output.value = "";
+});
+function setTranslate(value) {
+  let translated = null;
+  console.log(value);
+  const url = `https://api.funtranslations.com/translate/yoda.json?text=${value}`;
+
+  fetch(url)
+    .then((res) => {
+      if (res.ok) return res.json();
+      else throw new Error("Made too many requests wait for 1 hour");
+    })
+    .then((res) => {
+      translated = res.contents.translated;
+      output.value = translated;
+    })
+    .catch((err) => alert(err.message));
+}
